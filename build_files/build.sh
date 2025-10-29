c1777dd2d173a578e78fbbd36aed459be09eca35
tired-runner <19860054+tired-runner@users.noreply.github.com>
2025-10-29 15:10:35 -0400

    another shot

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,8 @@
 
 set -ouex pipefail
 
-cd /ctx/ryzen_smu
-make dkms-install
-
+# fuck it use a copr, who needs the package to be maintained anyway.
+# probably want to build the maintained one from source at some point, but I am using an old cpu with old agesa, don't really need new features
+dnf5 -y copr enable birkch/ryzen_smu
+dnf5 -y install dkms-ryzen_smu
+dnf5 -y copr disable birkch/ryzen_smu
